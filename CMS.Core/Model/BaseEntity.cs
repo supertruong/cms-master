05c204472f2e518e64670167573c0159fcc9e9ca
Le Nhat Truong (GES.DMS) <TruongLN1@fsoft.com.vn>
2019-07-24 14:13:24 +0700

    init source

---
+++ CMS.Core/Model/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Core.Model
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ CMS.Data/Repository/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using CMS.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Data.Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Delete(int id);
+        TEntity Insert(TEntity entity);
+        int InsertAndGetId(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
